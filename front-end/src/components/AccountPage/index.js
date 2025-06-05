8fa7aafd3c426b694bc1b175aad6765f7a1f7810
chandrasekhar <chandrasekharsathavalli99@gmail.com>
2025-06-06 01:51:24 +0530

    updated

--- front-end/src/components/AccountPage/index.js
+++ front-end/src/components/AccountPage/index.js
@@ -5,7 +5,9 @@ const AccountPage = () => {
   return (
     <div className="account-page-container">
       <div className="account-page">
-        <h1 className="account-title">Account Settings</h1>
+        <div className="account-title-container">
+          <h1 className="account-title">Account Settings</h1>
+        </div>
         <p className='account-description'>Lorem Ipsum Dolor Sit Amet, Consetetur Sadipscing Elitr, Sed Diam Nonumy Eirmod Tempor Invidunt Ut Labore Et Dolore Magna Aliquyam Erat, Sed Diam</p>
       </div>
     </div>
--- front-end/src/components/SignPage/index.js
+++ front-end/src/components/SignPage/index.js
@@ -5,12 +5,29 @@ const SignPage = () => {
   return (
     <div className="sign-page-container">
       <div className="sign-page">
-        <h1 className="sign-title">Sign In to your PopX account</h1>
+        <h1 className="sign-page-title">Sign In to your PopX account</h1>
         <p className='welcome-description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
-        <input
-          type="email"/>
-        <input
-          type="password"/> 
+        <form>
+          <div className="sign-in-wrapper">
+              <label htmlFor="email" className='sign-in-input-label'>Email address<span className='create-account-label-span'>*</span></label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Enter Email address"
+                className="input-field"
+              />
+            </div>
+            <div className="sign-in-wrapper">
+              <label htmlFor="password" className='sign-in-input-label'>Password<span className='create-account-label-span'>*</span></label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Enter Password"
+              className="input-field"
+            />
+            </div>
+        </form>
+      
         <Link to="/account">
           <button type="button" className="sign-in-button">Login</button>
         </Link>
